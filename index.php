ce60a611a592870975b43f78ad882bb376f02456
thachle <thachlh@nal.vn>
2019-07-27 21:04:43 +0700

    add google index

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="google-site-verification" content="dgDxlF_6TruTHJrsM63Cbr36jRqKwTiTvCANZh8qwng">
     <link rel="shortcut icon" href="images/favicon.ico">
     <link rel="stylesheet" href="css/bundle.css">
     <link rel="stylesheet" href="css/hody-icons.css">
